9ed36631a4b7506bfb62be9cfa587ee6541cef3e
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2024-08-02 15:14:15 +0800

    block states1

--- src/main/java/com/besson/retutotial/datagen/ModModelsProvider.java
+++ src/main/java/com/besson/retutotial/datagen/ModModelsProvider.java
@@ -38,7 +38,8 @@ public class ModModelsProvider extends FabricModelProvider {
         blockStateModelGenerator.registerNorthDefaultHorizontalRotation(ModBlocks.BOX);
         blockStateModelGenerator.registerSimpleState(ModBlocks.POLISHING_MACHINE);
         // 生成简单的方块状态文件，不生成模型文件
-        blockStateModelGenerator.registerSimpleState(ModBlocks.SIMPLE_BLOCK);
+//        blockStateModelGenerator.registerSimpleState(ModBlocks.SIMPLE_BLOCK);
+        blockStateModelGenerator.registerNorthDefaultHorizontalRotation(ModBlocks.SIMPLE_BLOCK);
     }
 
     @Override
